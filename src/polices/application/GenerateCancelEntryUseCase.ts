00e7c84cfb48128f985c0f5bbd314151bc124fac
Julian David Toro <jtoro@tecmovin.com>
2022-04-04 17:37:30 -0500

    Cancell with payments

--- src/polices/application/GenerateCancelEntryUseCase.ts
+++ src/polices/application/GenerateCancelEntryUseCase.ts
@@ -1,26 +1,35 @@
 import DaysBetween from "../domain/DaysBetween";
 import Entry from "../domain/entry";
-import { CHANGE_UNEARNED_PREMIUM_RESERVE, DIRECT_PREMIUMS_WRITTEN, EARNED_SURPLUS, FEE_REVENUE, FIGA_FEE_RECEIVABLE, FIGA_FEE_REVENUE, PREMIUMS_RECEIVED_IN_ADVANCED, PREMIUM_RECEIVABLE, REFUNS_PAYABLE, STATE_TAX_RECEIVABLE, STATE_TAX_REVENUE, UNEARNED_PREMIUM_RESERVE, UNEARNED_SURPLUS } from "../domain/ledger";
+import { CHANGE_UNEARNED_PREMIUM_RESERVE, DEFERRED_INSTALLMENTS, DIRECT_PREMIUMS_WRITTEN, EARNED_SURPLUS, FEE_REVENUE, FIGA_FEE_RECEIVABLE, FIGA_FEE_REVENUE, PREMIUMS_RECEIVED_IN_ADVANCED, PREMIUM_RECEIVABLE, REFUNS_PAYABLE, STATE_TAX_RECEIVABLE, STATE_TAX_REVENUE, UNASSIGNED_SURPLUS, UNEARNED_PREMIUM_RESERVE, UNEARNED_SURPLUS } from "../domain/ledger";
 import Policy from "../domain/policy";
 import { Cancelled } from "../domain/policy_event";
 
 export default class GenerateCancelEntryUseCase {
 
-    
-    // deferred
-    // remaining_unearned_premium_reserve
-    // remaining_unearned_surplus
-
-    // non_transactional_fees
     execute(policy: Policy, event: Cancelled, approvedPaymentsAmountBeforeInception: number, approvedPaymentsAmountAfterInception: number) : Entry []{   
+        const remainingPremium = Math.abs(policy.premium()/365 * DaysBetween.get(event.effective, policy.endDate()))
+        const remainingSurplus = Math.abs(policy.surplus()/365 * DaysBetween.get(event.effective, policy.endDate()))
+
+        const remainingStateTax = Math.abs(policy.taxFee()/365 * DaysBetween.get(event.effective, policy.endDate()))
+        const remainingFigaFee = Math.abs(policy.figaFee()/365 * DaysBetween.get(event.effective, policy.endDate()))
+
+        const remainingUnearnedPremiumReserve = Math.abs(policy.premium()/365 * DaysBetween.get(event.created >= event.effective ? event.created : event.effective, policy.endDate()))
+        const remainingUnearnedSurplus = Math.abs(policy.surplus()/365 * DaysBetween.get(event.created >= event.effective ? event.created : event.effective, policy.endDate()))
 
         const entries: Entry [] = [
-            this.getEntryForNonpayment(policy, event)
+            this.getEntryForNonpayment(
+                approvedPaymentsAmountBeforeInception,
+                approvedPaymentsAmountAfterInception,
+                remainingPremium,
+                remainingStateTax,
+                remainingFigaFee,
+                remainingUnearnedPremiumReserve,
+                remainingUnearnedSurplus
+            )
         ]
         
-        /*const refundPaymentEntry : Entry | undefined = this.getRefundPaymentEntry(
+        const refundPaymentEntry : Entry | undefined = this.getRefundPaymentEntry(          
             policy,
-            event,            
             approvedPaymentsAmountBeforeInception,
             approvedPaymentsAmountAfterInception,
             remainingPremium,
@@ -30,38 +39,38 @@ export default class GenerateCancelEntryUseCase {
         );
 
         if(refundPaymentEntry!==undefined){
-            //entries.push(refundPaymentEntry)
-        }*/
+            entries.push(refundPaymentEntry)
+        }
 
         return entries
     }
 
-    getEntryForNonpayment(policy: Policy, event: Cancelled) : Entry {
-        const remainingPremium = Math.abs(policy.premium()/365 * DaysBetween.get(event.effective, policy.endDate()))
-        //const remainingSurplus = Math.abs(policy.surplus()/365 * DaysBetween.get(event.effective, policy.endDate()))
-
-        const remainingStateTax = Math.abs(policy.taxFee()/365 * DaysBetween.get(event.effective, policy.endDate()))
-        const remainingFigaFee = Math.abs(policy.figaFee()/365 * DaysBetween.get(event.effective, policy.endDate()))
-        
-        const remainingUnearnedPremiumReserve = Math.abs(policy.premium()/365 * DaysBetween.get(event.created >= event.effective ? event.created : event.effective, policy.endDate()))
-        const remainingUnearnedSurplus = Math.abs(policy.surplus()/365 * DaysBetween.get(event.created >= event.effective ? event.created : event.effective, policy.endDate()))
-
+    getEntryForNonpayment( 
+        approvedPaymentsAmountBeforeInception: number, 
+        approvedPaymentsAmountAfterInception: number,
+        remainingPremium: number,
+        remainingStateTax: number,
+        remainingFigaFee: number,
+        remainingUnearnedPremiumReserve: number,
+        remainingUnearnedSurplus: number
+    ) : Entry {                        
         // Deal with multipay, 1 Pay don't deal with write off
+        const paymentAmount = approvedPaymentsAmountBeforeInception + approvedPaymentsAmountAfterInception;
 
         const debits : [string, number] [] = [
             [UNEARNED_PREMIUM_RESERVE, remainingUnearnedPremiumReserve],
             [UNEARNED_SURPLUS, remainingUnearnedSurplus],
             [DIRECT_PREMIUMS_WRITTEN, remainingPremium],
-            [STATE_TAX_REVENUE, remainingStateTax],
-            [FIGA_FEE_REVENUE, remainingFigaFee]
+            [STATE_TAX_REVENUE, paymentAmount===0 ? remainingStateTax : 0 ],
+            [FIGA_FEE_REVENUE, paymentAmount===0 ? remainingFigaFee: 0 ]
         ]
 
         const credits : [string, number] [] = [
             [CHANGE_UNEARNED_PREMIUM_RESERVE, remainingUnearnedPremiumReserve],
             [PREMIUM_RECEIVABLE, remainingPremium],
             [EARNED_SURPLUS, remainingUnearnedSurplus],
-            [STATE_TAX_RECEIVABLE, remainingStateTax],
-            [FIGA_FEE_RECEIVABLE, remainingFigaFee]
+            [STATE_TAX_RECEIVABLE, paymentAmount===0 ? remainingStateTax : 0],
+            [FIGA_FEE_RECEIVABLE, paymentAmount===0 ? remainingFigaFee: 0]
         ]
 
         return new Entry(
@@ -73,7 +82,6 @@ export default class GenerateCancelEntryUseCase {
 
     getRefundPaymentEntry(
         policy: Policy,
-        event: Cancelled,
         approvedPaymentsAmountBeforeInception: number, 
         approvedPaymentsAmountAfterInception: number,
         remainingPremium: number,
@@ -81,24 +89,22 @@ export default class GenerateCancelEntryUseCase {
         remainingStateTax: number,
         remainingFigaFee: number
     ) : Entry | undefined {
+        const totalApprovedPayment = (approvedPaymentsAmountBeforeInception+approvedPaymentsAmountAfterInception)
         
-        if((approvedPaymentsAmountBeforeInception+approvedPaymentsAmountAfterInception)===0){
+        if(totalApprovedPayment===0){
             return undefined;
         }
 
-        const prorratedSurplus = policy.surplus()/365 * DaysBetween.get(policy.effectiveDate(), event.effective)
-
         const debits : [string, number] [] = [
-            [PREMIUM_RECEIVABLE, approvedPaymentsAmountAfterInception + approvedPaymentsAmountAfterInception ],
+            [PREMIUM_RECEIVABLE, remainingPremium ],
             //[PREMIUMS_RECEIVED_IN_ADVANCED, ],
-            [FIGA_FEE_RECEIVABLE, remainingFigaFee],
-            [STATE_TAX_RECEIVABLE, remainingStateTax],
+            [UNASSIGNED_SURPLUS, remainingSurplus],
+            [FIGA_FEE_REVENUE, remainingFigaFee],
+            [STATE_TAX_REVENUE, remainingStateTax],
         ]
         const credits : [string, number] [] = [
-            //[FIGA_FEE_REVENUE, remainingFigaFee],
-            //[STATE_TAX_REVENUE, remainingStateTax],
-            [FEE_REVENUE, prorratedSurplus],
-            [REFUNS_PAYABLE, remainingPremium + remainingSurplus + remainingStateTax + remainingFigaFee]
+            [REFUNS_PAYABLE, remainingPremium + remainingSurplus + remainingStateTax + remainingFigaFee], // less deferred installments
+            //[DEFERRED_INSTALLMENTS, ]
         ]
 
         return new Entry(
